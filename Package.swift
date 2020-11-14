e3cdf607c88d770fb427c3a1ffc750a322a68ec5
Changemin <bkm.change.min@gmail.com>
2020-11-14 21:35:07 +0900

    Add 'Dependencies'

--- Package.swift
+++ Package.swift
@@ -14,6 +14,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.2.2"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from :"5.15.7")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # DimigoinKit
 
-A description of this package.
+디미고인의 iOS/macOS 개발을 위한 DimigoinKit
+
+
