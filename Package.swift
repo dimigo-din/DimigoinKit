4ddb220b438f4abeb0116d9fe38c51c38eaafef2
Changemin <bkm.change.min@gmail.com>
2021-01-07 13:24:33 +0900

    Modified 'package.swift'

--- Package.swift
+++ Package.swift
@@ -10,26 +10,18 @@ let package = Package(
         .macOS(.v11)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DimigoinKit",
             targets: ["DimigoinKit"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire", from: "5.2.2"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.0"),
         .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI.git", from: "1.5.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DimigoinKit",
-            dependencies: ["Alamofire", "SwiftyJSON", "SDWebImageSwiftUI"]),
-        .testTarget(
-            name: "DimigoinKitTests",
-            dependencies: ["DimigoinKit"]),
+            dependencies: ["Alamofire", "SwiftyJSON", "SDWebImageSwiftUI"])
     ]
 )
