8aa8b7ff84a7276b2e5c7a5e1ce48fdebba3611f
Changemin <bkm.change.min@gmail.com>
2021-01-26 19:50:54 +0900

    Update 'TokenAPI'

--- Sources/DimigoinKit/Models/Token.swift
+++ Sources/DimigoinKit/Models/Token.swift
@@ -1,78 +1,51 @@
 //
-//  File.swift
-//  
+//  TokenAPI.swift
+//  DimigoinKit
 //
-//  Created by 변경민 on 2020/11/15.
+//  Created by 변경민 on 2021/01/26.
 //
 
-import SwiftUI
+import Foundation
 import Alamofire
 import SwiftyJSON
 
-/// token의 존재 여부 (=로그인 이력)
-public enum TokenStatus {
-    case exist
-    case none
-}
-
 public enum TokenError: Error {
     case authFailed
+    case firstLogin
     case unknown
+    case emptyArguments
 }
 
-/// 디미고인 유저 토큰 관련
-public class TokenAPI: ObservableObject {
-    @Published public var accessToken: String = ""
-    @Published public var refreshToken: String = ""
-    @Published public var tokenStatus: TokenStatus = .none
-    
-    private var username: String = ""
-    private var password: String = ""
-    
-    public init() {
-        checkTokenStatus()
-        saveTokens()
-    }
-    
-    /// Username과 Password를 설정
-    public func setUsernamePassword(username: String, password: String) -> Void{
-        self.username = username
-        self.password = password
-    }
-    
-    /// https://edison.dimigo.hs.kr/auth
-    /// 토큰을 가져옵니다. ([POST] /auth)
-    public func getTokens() -> Void {
-        LOG("get token")
-        let parameters: [String: String] = [
-            "username": "\(self.username)",
-            "password": "\(self.password)"
+public struct TokenAPI {
+    /// 토큰을 새로고침 합니다.
+    func fetchTokens(_ refreshToken:String, completion: @escaping (Result<(accessToken: String, refreshToken: String), TokenError>) -> Void) {
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer \(refreshToken)"
         ]
-        let endPoint = "/auth"
+        let endPoint = "/auth/refresh"
         let method: HTTPMethod = .post
-        AF.request(rootURL+endPoint, method: method, parameters: parameters, encoding: JSONEncoding.default).response { response in
+        AF.request(rootURL+endPoint, method: method, encoding: JSONEncoding.default, headers: headers).response { response in
             if let status = response.response?.statusCode {
                 switch(status) {
                 case 200:
                     let json = JSON(response.value!!)
-                    self.accessToken = json["accessToken"].string!
-                    self.refreshToken = json["refreshToken"].string!
-                    self.debugToken()
-                    self.saveTokens()
-                    self.tokenStatus = .exist
+                    let accessToken = json["accessToken"].string!
+                    let refreshToken = json["refreshToken"].string!
+                    self.saveTokens(accessToken, refreshToken)
+                    completion(.success((accessToken, refreshToken)))
+                case 400:
+                    completion(.failure(.emptyArguments))
+                case 401:
+                    completion(.failure(.authFailed))
                 default:
-                    LOG("get token failed")
-                    if debugMode {
-                        debugPrint(response)
-                    }
-                    self.tokenStatus = .none
+                    print("refresh token failed")
                 }
             }
         }
     }
     
-    public func getTokens(_ username: String, _ password: String, completion: @escaping (Result<(accessToken: String, refreshToken: String), TokenError>) -> Void) {
-        LOG("get token")
+    /// username과 password로 토큰을 발급받습니다.
+    func fetchTokens(_ username: String, _ password: String, completion: @escaping (Result<(accessToken: String, refreshToken: String), TokenError>) -> Void) {
         let parameters: [String: String] = [
             "username": "\(username)",
             "password": "\(password)"
@@ -86,110 +59,39 @@ public class TokenAPI: ObservableObject {
                     let json = JSON(response.value!!)
                     let accessToken = json["accessToken"].string!
                     let refreshToken = json["refreshToken"].string!
-                    self.debugToken()
-                    self.saveTokens()
-                    self.tokenStatus = .exist
+                    self.saveTokens(accessToken, refreshToken)
                     completion(.success((accessToken, refreshToken)))
-                case 401: completion(.failure(.authFailed))
+                case 401:
+                    completion(.failure(.authFailed))
                 default: completion(.failure(.unknown))
                 }
             }
         }
     }
     
-    /// http://edison.dimigo.hs.kr/auth/refresh
-    /// 토큰을 새로고침 합니다.([POST] /auth/refresh)
-    public func refreshTokens() {
-        LOG("refresh tokens")
-        let headers: HTTPHeaders = [
-            "Authorization": "Bearer \(self.refreshToken)"
-        ]
-        let endPoint = "/auth/refresh"
-        let method: HTTPMethod = .post
-        AF.request(rootURL+endPoint, method: method, encoding: JSONEncoding.default, headers: headers).response { response in
-            if let status = response.response?.statusCode {
-                switch(status) {
-                case 200:
-                    let json = JSON(response.value!!)
-                    self.accessToken = json["accessToken"].string!
-                    self.refreshToken = json["refreshToken"].string!
-                    self.debugToken()
-                    self.saveTokens()
-                    withAnimation() {
-                        self.tokenStatus = .exist
-                    }
-                case 400:
-                    LOG("어딧냐 토큰??")
-                default:
-                    LOG("refresh token failed")
-                    if debugMode {
-                        debugPrint(response)
-                    }
-                }
-            }
-        }
-    }
-
-    /// 토큰을 출력합니다.
-    public func debugToken() {
-        LOG("accessToken : \(accessToken)")
-        LOG("refreshToken : \(refreshToken)")
+    /// 기기에 토큰을 저장합니다.
+    func saveTokens(_ accessToken: String, _ refreshToken: String) {
+        UserDefaults.standard.setValue(accessToken, forKey: "accessToken")
+        UserDefaults.standard.setValue(refreshToken, forKey: "refreshToken")
     }
     
-    /// 토큰을 기기에 저장합니다.
-    public func saveTokens() {
-        LOG("save tokens")
-        UserDefaults.standard.setValue(self.accessToken, forKey: "accessToken")
-        UserDefaults.standard.setValue(self.refreshToken, forKey: "refreshToken")
+    /// 저장된 토큰을 불러옵니다. 없다면 TokenError.firstLogin 에러를 반환합니다.
+    func loadSavedTokens(completion: @escaping (Result<(accessToken: String, refreshToken: String), TokenError>) -> Void) {
+        let accessToken: String = UserDefaults.standard.string(forKey: "accessToken") ?? ""
+        let refreshToken: String = UserDefaults.standard.string(forKey: "refreshToken") ?? ""
         
-        // for dimigoin App service only
-        UserDefaults(suiteName: appGroupName)?.setValue(self.accessToken, forKey: "accessToken")
-        UserDefaults(suiteName: appGroupName)?.setValue(self.refreshToken, forKey: "refreshToken")
-    }
-    
-    /// 저장된 토큰을 불러옵니다.
-    public func loadTokens() {
-        LOG("load tokens")
-        self.accessToken = UserDefaults.standard.string(forKey: "accessToken") ?? ""
-        self.refreshToken = UserDefaults.standard.string(forKey: "refreshToken") ?? ""
-    }
-    
-    /// 저장된 토큰 유무를 확인합니다. (= 로그인 이력 조회)
-    public func checkTokenStatus(){
-        if UserDefaults.standard.string(forKey: "accessToken") != nil && UserDefaults.standard.string(forKey: "refreshToken") != nil {
-            self.loadTokens()
-            self.tokenStatus = .exist
-        } else {
-            tokenStatus = .none
+        if(accessToken == "" && refreshToken == "") {
+            completion(.failure(.firstLogin))
+        }
+        else {
+            completion(.success((accessToken, refreshToken)))
         }
     }
     
-    /// 저장된 토큰을 삭제합니다. (로그아웃)
-    public func clearTokens() {
-        LOG("Remove tokens")
-        
+    /// 토큰을 삭제하고, 로그아웃합니다.
+    func removeTokens(completion: @escaping () -> Void) {
         UserDefaults.standard.removeObject(forKey: "accessToken")
         UserDefaults.standard.removeObject(forKey: "refreshToken")
-        
-        
-        // for dimigoin App service only
-        UserDefaults(suiteName: appGroupName)?.removeObject(forKey: "accessToken")
-        UserDefaults(suiteName: appGroupName)?.removeObject(forKey: "refreshToken")
-        withAnimation() {
-            self.tokenStatus = .none
-        }
+        completion()
     }
-    
-}
-
-
-/// 토큰을 기기에 저장합니다.
-public func saveTokens(_ accessToken: String, _ refreshToken: String) {
-    LOG("save tokens")
-    UserDefaults.standard.setValue(accessToken, forKey: "accessToken")
-    UserDefaults.standard.setValue(refreshToken, forKey: "refreshToken")
-    
-    // for dimigoin App service only
-    UserDefaults(suiteName: appGroupName)?.setValue(accessToken, forKey: "accessToken")
-    UserDefaults(suiteName: appGroupName)?.setValue(refreshToken, forKey: "refreshToken")
 }
