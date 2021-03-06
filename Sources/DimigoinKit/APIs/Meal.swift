a99b4d7217f77dc9e7a39e6a24bb2a229fd6aaef
Changemin <bkm.change.min@gmail.com>
2021-03-06 20:27:24 +0900

    Improve 'Meal Model'

--- Sources/DimigoinKit/APIs/Meal.swift
+++ Sources/DimigoinKit/APIs/Meal.swift
@@ -11,19 +11,43 @@ import SwiftyJSON
 
 /// 급식 모델
 public struct Meal {
-    public init(_ breakfast: String, _ lunch: String, _ dinner: String) {
+    public init(_ breakfast: [String], _ lunch: [String], _ dinner: [String]) {
         self.breakfast = breakfast
         self.lunch = lunch
         self.dinner = dinner
     }
     public init() {
-        self.breakfast = "급식 정보가 없습니다."
-        self.lunch = "급식 정보가 없습니다."
-        self.dinner = "급식 정보가 없습니다."
+        self.breakfast = []
+        self.lunch = []
+        self.dinner = []
     }
-    public var breakfast: String
-    public var lunch: String
-    public var dinner: String
+    public var breakfast: [String]
+    public var lunch: [String]
+    public var dinner: [String]
+    
+    public func getBreakfastString() -> String {
+        if breakfast.isEmpty {
+            return "급식 정보가 없습니다."
+        } else {
+            return bindingMenus(menu: breakfast)
+        }
+    }
+    
+    public func getLunchString() -> String {
+        if lunch.isEmpty {
+            return "급식 정보가 없습니다."
+        } else {
+            return bindingMenus(menu: lunch)
+        }
+    }
+    
+    public func getDinnerString() -> String {
+        if dinner.isEmpty {
+            return "급식 정보가 없습니다."
+        } else {
+            return bindingMenus(menu: dinner)
+        }
+    }
 }
 
 /// 급식 종류(아침, 점심, 저녁) Enum
@@ -34,6 +58,7 @@ public enum MealType {
 }
 
 public enum MealError: Error {
+    case invalidDate
     case alreadyExist
     case unknown
     case timeout
@@ -45,13 +70,51 @@ public func getMeal(from date: String, completion: @escaping (Meal) -> Void){
     let method: HTTPMethod = .get
     AF.request(rootURL+endPoint, method: method, encoding: JSONEncoding.default).responseData { response in
         let json = JSON(response.value ?? "")
-        completion(Meal(bindingMenus(menu: json["meal"]["breakfast"]),
-                        bindingMenus(menu: json["meal"]["lunch"]),
-                        bindingMenus(menu: json["meal"]["dinner"])))
+//        completion(Meal(bindingMenus(menu: json["meal"]["breakfast"]),
+//                        bindingMenus(menu: json["meal"]["lunch"]),
+//                        bindingMenus(menu: json["meal"]["dinner"])))
+        completion(json2Meal(json: json["meal"]))
     }
 }
 
-public func registerMeal(date: Date, meal: Meal, completion: @escaping (Result<[Notice], MealError>) -> Void) {
+public func json2Meal(json: JSON) -> Meal{
+    var meal: Meal = Meal()
+    for i in 0..<json["breakfast"].count {
+        meal.breakfast.append(json["breakfast"][i].string!)
+    }
+    for i in 0..<json["lunch"].count {
+        meal.lunch.append(json["lunch"][i].string!)
+    }
+    for i in 0..<json["dinner"].count {
+        meal.dinner.append(json["dinner"][i].string!)
+    }
+    return meal
+}
+
+public func registerMeal(accessToken: String, date: Date, meal: Meal, completion: @escaping (Result<Void, MealError>) -> Void) {
+    let headers: HTTPHeaders = [
+        "Authorization": "Bearer \(accessToken)"
+    ]
+    let parameters: [String:String] = [
+        "deviceToken": ""
+    ]
+    let endPoint = "/meal/date/\(getDateString(from: date))"
+    let method: HTTPMethod = .post
+    AF.request(rootURL+endPoint, method: method, parameters: parameters, encoding: JSONEncoding.default, headers: headers).response { response in
+        if let status = response.response?.statusCode {
+            switch(status) {
+            case 200:
+                completion(.success(()))
+            case 443:
+                completion(.failure(.alreadyExist))
+            default:
+                completion(.failure(.unknown))
+            }
+        }
+    }
+}
+
+public func patchMeal() {
     
 }
 
@@ -88,9 +151,9 @@ public func bindingMenus(menu: [String]) -> String{
 /// 급식 모델에서 끼니별로 급식을 반환합니다.
 public func getMealMenu(meal: Meal, _ mealType: MealType) -> String{
     switch mealType {
-        case .breakfast: return meal.breakfast
-        case .lunch: return meal.lunch
-        case .dinner: return meal.dinner
+        case .breakfast: return meal.getBreakfastString()
+        case .lunch: return meal.getLunchString()
+        case .dinner: return meal.getDinnerString()
     }
 }
 
@@ -113,7 +176,7 @@ public func getMealType() -> MealType {
 
 /// 예시 급식
 public let sampleMeal = Meal(
-    "카레라이스 | 쌀밥 | 콩나물국 | 너비아니조림 | 어묵야채볶음 | 포기김치 | 모듬과일",
-    "라면&보조밥 | 소떡소떡 | 야끼만두&초간장 | 참나물만다린무침 | 단무지 | 포기김치 | 우유빙수",
-    "김치참치마요덮밥 | 쌀밥 | 콩나물국 | 치즈스틱 | 실곤약치커리무침 | 깍두기 | 미니딸기파이"
+    ["카레라이스", "쌀밥", "콩나물국", "너비아니조림", "어묵야채볶음", "포기김치", "모듬과일"],
+    ["라면&보조밥", "소떡소떡", "야끼만두&초간장", "참나물만다린무침", "단무지", "포기김치", "우유빙수"],
+    ["김치참치마요덮밥", "쌀밥", "콩나물국", "치즈스틱", "실곤약치커리무침", "깍두기", "미니딸기파이"]
 )
