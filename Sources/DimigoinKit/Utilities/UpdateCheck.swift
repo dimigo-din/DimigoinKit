3aa5d70e1c634b712eb2bd4944859854a5c54c9d
Changemin <bkm.change.min@gmail.com>
2021-04-18 20:05:37 +0900

    Implement 'update check'

--- Sources/DimigoinKit/Utilities/UpdateCheck.swift
+++ Sources/DimigoinKit/Utilities/UpdateCheck.swift
@@ -21,7 +21,7 @@ public func needsUpdate() -> Bool {
             if let appStoreVersion = results[0]["version"] as? String{
                 let currentVersion = infoDictionary!["CFBundleShortVersionString"] as? String
                 if !(appStoreVersion == currentVersion) {
-                    print("Need to update [\(appStoreVersion) != \(currentVersion)]")
+                    print("Need to update [\(appStoreVersion) != \(currentVersion ?? "error")]")
                     return true
                 }
             }
@@ -30,3 +30,23 @@ public func needsUpdate() -> Bool {
     return false
 }
 
+public func getLatestAppVersion() -> String {
+    let infoDictionary = Bundle.main.infoDictionary
+    let appID = infoDictionary!["CFBundleIdentifier"] as! String
+    let url = URL(string: "http://itunes.apple.com/lookup?bundleId=\(appID)")!
+    guard let data = try? Data(contentsOf: url) else {
+      return "error"
+    }
+    let lookup = (try? JSONSerialization.jsonObject(with: data , options: [])) as? [String: Any]
+    if let resultCount = lookup!["resultCount"] as? Int, resultCount == 1 {
+        if let results = lookup!["results"] as? [[String:Any]] {
+            if let appStoreVersion = results[0]["version"] as? String{
+                let currentVersion = infoDictionary!["CFBundleShortVersionString"] as? String
+                if !(appStoreVersion == currentVersion) {
+                    return appStoreVersion
+                }
+            }
+        }
+    }
+    return "0.0.0"
+}
